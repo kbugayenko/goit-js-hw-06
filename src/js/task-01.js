2b875b511758f5a2ce752d6e469d3dfa34d63cb5
kbugayenko <k.bugayenko@gmail.com>
2022-10-24 23:25:28 +0300

    update task-1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,8 @@
 const elem = document.querySelectorAll(".item");
 console.log("Number of categories:", elem.length);
-console.log("Category:", elem.textContent);
+console.log("");
+[...elem].forEach((el) => {
+  console.log("Category:", el.firstElementChild.textContent);
+  console.log("Elements:", el.lastElementChild.children.length);
+  console.log("");
+});
