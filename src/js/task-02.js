2f31dd62e5a15d5d424b189d136f214b73788f1b
kbugayenko <k.bugayenko@gmail.com>
2022-10-24 23:56:21 +0300

    update task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ul = document.querySelector("#ingredients");
+const arr = [];
+for (let i = 0; i < ingredients.length; i += 1) {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = ingredients[i];
+  arr.push(li);
+}
+ul.append(...arr);
