251f05ea7da3233d04075e2249c3e6ad88a17a0f
kbugayenko <k.bugayenko@gmail.com>
2022-10-28 01:17:50 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+"use strict";
 
+const refs = {
+  inputMail: document.querySelector("#mail"),
+  inputPass: document.querySelector("#password"),
+  submitEl: document.querySelector("#submit"),
+};
+
+refs.input.addEventListener("input", onInput);
+
+function onInput(event) {
+  event.preventDefault();
+}
