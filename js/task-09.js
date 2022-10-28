158d4e5006835e218f69095f8a042b216a2db4b7
kbugayenko <k.bugayenko@gmail.com>
2022-10-29 00:14:02 +0300

    update task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+"use strict";
+
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+btn.addEventListener("click", btnClick);
+
+function btnClick(evt) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
